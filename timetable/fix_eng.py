11715d0f1f260c4ada9fbf2ca135d5c3816d7532
astrovlad <102761503+astrovlad@users.noreply.github.com>
2022-11-08 18:39:28 +0300

    Update fix_eng.py

--- timetable/fix_eng.py
+++ timetable/fix_eng.py
@@ -1,16 +1,15 @@
 import pandas as pd
 
-def fix(df_):
-    df = df_.copy()
+def multiple_places_to_single(df_):
     for (weekday, group, subject, start), sub_df in df.groupby(['weekday', 'group', 'subject', 'start']):
-        if len(sub_df) > 1:
-            teachers = sub_df['teacher'].values
-            places = sub_df['place'].values
+        if len(sub_df) > 1: # если кол-во пар в одном месте в одно время больше одной
+            teachers = sub_df['teacher'].values # список всех преподавателей
+            places = sub_df['place'].values # список всех мест
             new_df = sub_df.iloc[0].copy()
-            new_df['teacher'] = ', '.join(teachers)
-            new_df['place'] = ', '.join(places)
+            new_df['teacher'] = ', '.join(teachers) # собираем все преподавателей в один список
+            new_df['place'] = ', '.join(places) # собираем все места в один список
 
             indexes = sub_df.index
-            df.drop(indexes, axis=0, inplace=True)
-            df.loc[len(df)] = new_df
+            df.drop(indexes, axis=0, inplace=True) # убираем старые ряды
+            df.loc[len(df)] = new_df # добавляем в исходную таблицу обновленный ряд
     return df
